692bb9ccb059f25b6300fcbd4c6291843b440bbf
hex <hexhex16@outlook.com>
2021-01-27 19:47:05 +0800

    2021-01-27_19:47:05

--- update.sh
+++ update.sh
@@ -1,15 +1,22 @@
-git pull origin main;
-git checkout main;
+
 # merge local master to main
 # git merge master;
-git add .;
-echo "[DEBUG] git merging ====================="
-git merge -v --no-ff -m "merge with no-ff" main
 
+git add .;
 var=`date "+%Y-%m-%d_%H:%M:%S"`
 # echo $var
 git commit -m $var;
+
+git pull origin main;
+git checkout main;
+
+echo "[DEBUG] git merging ====================="
+git merge -v --no-ff -m "merge with no-ff" main
+
+
 git push origin main
 
 
 # 啥玩意儿 测试一下
+
+# In Onedrive
