2c076251cb59bf0f365de45494ee04fb38c4e414
hex <hexhex16@outlook.com>
2021-01-27 20:10:03 +0800

    2021-01-27_20:10:03

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # merge local master to main
 # git merge master;
 
-git add .;
+git add . -v;
 var=`date "+%Y-%m-%d_%H:%M:%S"`
 # echo $var
 git commit -m $var;
@@ -14,7 +14,7 @@ echo "[DEBUG] git merging ====================="
 git merge -v --no-ff -m "merge with no-ff" main
 
 
-git push origin main
+git push -v origin main
 
 
 # 啥玩意儿 测试一下
