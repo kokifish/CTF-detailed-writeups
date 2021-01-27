73c42b4a6ba75be14fa0b4cc640850044abe7716
hex <hexhex16@outlook.com>
2021-01-27 21:13:27 +0800

    2021-01-27_21:13:27

--- update.sh
+++ update.sh
@@ -1,26 +1,16 @@
-
-# merge local master to main
-# git merge master;
-
-git checkout main;
+git checkout main -q;
 
 git add . -v;
 var=`date "+%Y-%m-%d_%H:%M:%S"`
 # echo $var
 git commit -am $var;
 
-echo "[DEBUG] git pull ====================="
-git pull origin main;
+echo "[DEBUG] git pull origin main START ==="
+git pull -v origin main;
 
-
-echo "[DEBUG] git merge ====================="
-git merge -v --no-ff -m "merge with no-ff" main
+echo "[WARNING] If conflict occured, related files will show below:"
+git status
+echo "[WARNING] Fix by hands if conflict exits."
 
 echo "[DEBUG] git push ====================="
 git push -v origin main
-
-# WorkSpace 这时remote显示的是 # Onedrive TEST 22222 这里改了这一行 或者说删了 增加了这一行
-
-
-# WorkSpace 并且还有一行新的
-# Onedrive TEST 22222
