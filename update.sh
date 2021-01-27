446cc2423189e21e82a5291c62244fc1671c1007
hex <hexhex16@outlook.com>
2021-01-27 20:05:23 +0800

    2021-01-27_20:05:23

--- update.sh
+++ update.sh
@@ -2,13 +2,15 @@
 # merge local master to main
 # git merge master;
 
+git checkout main;
+
 git add .;
 var=`date "+%Y-%m-%d_%H:%M:%S"`
 # echo $var
 git commit -am $var;
 
 git pull origin main;
-git checkout main;
+
 
 echo "[DEBUG] git merging ====================="
 git merge -v --no-ff -m "merge with no-ff" main
