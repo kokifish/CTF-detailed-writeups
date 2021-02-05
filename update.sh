38bcee89f5e04b684e950a7b8dee715ba2895647
hex <hexhex16@outlook.com>
2021-02-05 17:40:29 +0800

    2021-02-05_17:40:29

--- update.sh
+++ update.sh
@@ -1,21 +1,18 @@
-echo "[Step-1] Local changes add and commit:"
-
-git checkout main -q;
-git add . -v;
-var=`date "+%Y-%m-%d_%H:%M:%S"`
-# echo $var
-git commit -am $var;
-
-echo 
-echo "[Step-2] git pull origin main:"
+echo "[Step-1] git pull origin main:"
 git pull origin main;
-
 echo 
 echo "=== [WARNING] If conflict occured, related files will show below: ========="
 git status
 echo "=== [WARNING] Fix by hands if conflict exits. Then [RE-RUN] this script ==="
 echo 
-echo 
+
+echo "[Step-2] Local changes add and commit:"
+git checkout main;
+git add . -v;
+var=`date "+%Y-%m-%d_%H:%M:%S"`
+# echo $var
+git commit -am $var;
+
 
 echo "[Step-3] git push origin main:"
 git push origin main
